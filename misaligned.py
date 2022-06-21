8ba6c934e8f0ed0a99245f0ac2d4d14815fec392
Kalburgi Abhishek Nagosa (SX/BSV-TC4) <AbhishekNagosa.Kalburgi@in.bosch.com>
2022-06-22 00:26:49 +0530

    Modified the code to make the tests fail

--- misaligned.py
+++ misaligned.py
@@ -4,10 +4,17 @@ def print_color_map():
     minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
     for i, major in enumerate(major_colors):
         for j, minor in enumerate(minor_colors):
-            print(f'{i * 5 + j} | {major} | {minor}')
+            s = formatter(i*5 + j, major, minor)
+            print(s)
     return len(major_colors) * len(minor_colors)
+
+
+def formatter(num, major, minor):
+    s = "{:<3}| {:<10}| {:<10}".format(str(num), major, minor)
+    return s
 
 
 result = print_color_map()
 assert(result == 25)
+assert(formatter(1, 'White', 'Orange').find("|") == 3)
 print("All is well (maybe!)\n")
