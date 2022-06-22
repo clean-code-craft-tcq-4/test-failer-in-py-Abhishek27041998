8559f458691e9374f81861806ba65decda53e395
Kalburgi Abhishek Nagosa (SX/BSV-TC4) <AbhishekNagosa.Kalburgi@in.bosch.com>
2022-06-22 14:06:32 +0530

    Modified Misaligned to separate functions to test num starting from 1

--- misaligned.py
+++ misaligned.py
@@ -4,9 +4,14 @@ def print_color_map():
     minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
     for i, major in enumerate(major_colors):
         for j, minor in enumerate(minor_colors):
-            s = formatter(i*5 + j, major, minor)
+            num = generate_next_number(i, j)
+            s = formatter(num, major, minor)
             print(s)
     return len(major_colors) * len(minor_colors)
+
+
+def generate_next_number(i, j):  # Separating the function for testing
+    return i*5 + j
 
 
 def formatter(num, major, minor):
@@ -17,5 +22,5 @@ def formatter(num, major, minor):
 result = print_color_map()
 assert(result == 25)
 assert(formatter(10, 'White', 'Orange').find("|") == 2)
-
+assert(generate_next_number(0, 0) == 1)  # First number is 1 but this implementation returns 0
 print("All is well (maybe!)\n")
